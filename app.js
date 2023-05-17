5f80b9564fabc2d8a072dcac97acccf90465d9ad
chun0Hsu <chunlin1022@gmail.com>
2023-05-17 14:09:45 +0800

    models

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const db = require('./config/mongoose')
+const Url = require('./models/url')
 
 app = express()
 port = 3000
@@ -8,8 +9,14 @@ port = 3000
 app.engine('handlebars', exphbs.engine({ default: "main" }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
 })
 
 app.listen(port, () => {
