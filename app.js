4b31d3550fca8bd0051e509fd3f5e56fec3c6d02
chun0Hsu <chunlin1022@gmail.com>
2023-05-17 22:05:49 +0800

    confirm url exists

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const axios = require('axios')
 const db = require('./config/mongoose')
 const Url = require('./models/url')
+const shortenFunc = require('./shortenFunc')
 
 app = express()
 port = 3000
@@ -11,12 +13,34 @@ app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
 
+//檢查網址存在
+const checkUrlExists = async (req, res, next) => {
+  const url = req.body.originURL;
+
+  try {
+    const response = await axios.head(url);
+    if (response.status === 200) {
+
+      next();
+    } else {
+
+      res.status(404).json({ error: 'URL does not exist' });
+    }
+  } catch (error) {
+
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.post('/', (req, res) => {
-  console.log(req.headers)
+app.post('/', checkUrlExists, (req, res) => {
+  const originURL = req.body.originURL
+  const shortURL = shortenFunc()
+
+  
 })
 
 app.listen(port, () => {
--- models/url.js
+++ models/url.js
@@ -4,7 +4,11 @@ const urlSchema = new Schema({
   url: {
     type: String,
     required: true
+  },
+  shortUrl: {
+    type: String,
+    required: true
   }
 })
 
-module.exports = mongoose.model('url', urlSchema)
+module.exports = mongoose.model('URL', urlSchema)
